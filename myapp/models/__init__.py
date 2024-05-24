c7e5a7d3c29161a473c307555af333a5c434d0c7
NiveaClaro <niveaclaro@yahoo.com.br>
2024-05-24 17:13:13 -0300

    DRAFT cadastro de agenda

--- myapp/models/__init__.py
+++ myapp/models/__init__.py
@@ -1,4 +1,4 @@
 from .turma import Turma
-
 from .hora_aula import Hora_aula
 from .disciplina import Disciplina
+from .export_models import Turma, Grade, Hora_aula, Disciplina, Horario, Agenda
---
+++ myapp/models/horario.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Horario(models.Model):
+    inicio = models.TimeField()
+    fim = models.TimeField()
+
+    def __str__(self):
+        return f"{self.inicio} - {self.fim}"
