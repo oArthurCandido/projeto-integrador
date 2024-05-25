77e0b6870526b526671a27df42427aab94b9e3a6
NiveaClaro <niveaclaro@yahoo.com.br>
2024-05-25 09:22:20 -0300

    draft avisos

--- myapp/models/export_models.py
+++ myapp/models/export_models.py
@@ -9,5 +9,6 @@ from myapp.models.responsavel import Responsavel
 from myapp.models.turma import Turma
 from myapp.models.horario import Horario
 from myapp.models.agenda import Agenda
+from myapp.models.avisos import Avisos
 from myapp.models.user_turma import User_Turma
-__all__ = ['Aluno', 'Disciplina', 'Endereco', 'Grade', 'Parentesco', 'Hora_aula', 'Professor', 'Responsavel', 'Turma', 'Horario', 'Agenda', 'User_Turma']
+__all__ = ['Aluno', 'Disciplina', 'Endereco', 'Grade', 'Parentesco', 'Hora_aula', 'Professor', 'Responsavel', 'Turma', 'Horario', 'Agenda', 'User_Turma', 'Avisos']
